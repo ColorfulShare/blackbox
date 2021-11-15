8c22d821107b320f489abde68a4cc432993781ba
juliozn5 <juliocesarzn5@gmail.com>
2021-11-15 18:16:03 -0400

    Update page-profile.blade.php

--- resources/views/content/pages/page-profile.blade.php
+++ resources/views/content/pages/page-profile.blade.php
@@ -271,8 +271,8 @@
 
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+          <button type="submit" class="btn btn-primary">Guardar</button>
         </div>
       </div>
     </div>
