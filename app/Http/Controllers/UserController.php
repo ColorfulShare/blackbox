9202ea4af970d74ea4803e44dbb3ddad59193950
juliozn5 <juliocesarzn5@gmail.com>
2021-11-16 17:57:36 -0400

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,9 +18,10 @@ use App\Notifications\userActivacionExitosa;
 use App\Models\OrdenPurchase;
 use App\Http\Traits\Tree;
 use Illuminate\Database\Eloquent\Collection;
-
+use App\Http\Traits\TwoFactor;
 class UserController extends Controller
 {
+    use TwoFactor;
     use Tree;
 
     public function __construct()
